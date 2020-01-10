d61be30be0513d86336e12f3915af20fe3bb9037
Michal Zmudziak <Michal.Zmudziak@ibm.com>
2020-01-10 14:23:52 +0100

    refactor

--- cypress/integration/crawl.spec.js
+++ cypress/integration/crawl.spec.js
@@ -3,10 +3,6 @@ context('Crawl', () => {
   const data = [];
   Array.from({ length: 184 }, (value, index) => index + 1).forEach(num =>
     it(`crawls page num ${num}`, () => {
-      const page = {
-        index: num,
-        elements: []
-      };
       cy.visit(baseUrl + num);
       cy.get('ul.wizResBox>li').each(($rows, index) => {
         const element = {
@@ -16,15 +12,14 @@ context('Crawl', () => {
         element.address.street = $rows.find('div[itemprop="streetAddress"]').text();
         element.address.postalCode = $rows.find('span[itemprop="postalCode"]').text();
         element.address.city = $rows.find('span[itemprop="addressLocality"]').text();
-        element.address.county = $rows.find('span[itemprop="addressRegion"]').text();
+        element.address.region = $rows.find('span[itemprop="addressRegion"]').text();
         element.phone = $rows.find('div[itemprop="telephone"]').text();
-        page.elements.push(element)
+        data.push(element)
       });
-      data.push(page);
       cy.wait(500)
     }));
 
   after(() => {
-    cy.writeFile('lol.json', data);
+    cy.writeFile('result.json', data);
   })
 });
