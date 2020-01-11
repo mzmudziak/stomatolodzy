feba699358c566a0492f67fa84102b29d1a50a0d
Michal Zmudziak <Michal.Zmudziak@ibm.com>
2020-01-11 13:55:08 +0100

    remove result

--- cypress/integration/crawl.spec.js
+++ cypress/integration/crawl.spec.js
@@ -4,21 +4,21 @@ context('Crawl', () => {
   Array.from({ length: 184 }, (value, index) => index + 1).forEach(num =>
     it(`crawls page num ${num}`, () => {
       cy.visit(baseUrl + num);
-      cy.get('ul.wizResBox>li').each(($rows, index) => {
+      cy.get('ul.wizResBox>li').each(($row, index) => {
         data.push({
-          name: $rows.find('.przeppoz').text(),
-          phone: $rows.find('div[itemprop="telephone"]').text(),
+          name: $row.find('.przeppoz').text(),
+          phone: $row.find('div[itemprop="telephone"]').text(),
           address: {
-            street: $rows.find('div[itemprop="streetAddress"]').text(),
-            postalCode: $rows.find('span[itemprop="postalCode"]').text(),
-            city: $rows.find('span[itemprop="addressLocality"]').text(),
-            region: $rows.find('span[itemprop="addressRegion"]').text()
+            street: $row.find('div[itemprop="streetAddress"]').text(),
+            postalCode: $row.find('span[itemprop="postalCode"]').text(),
+            city: $row.find('span[itemprop="addressLocality"]').text(),
+            region: $row.find('span[itemprop="addressRegion"]').text()
           }
         })
       });
-      cy.wait(500)
+      cy.wait(500);
     }));
-
+s
   after(() => {
     cy.writeFile('result.json', data);
   })
